16135715458e8f9d937a43db9ca0fc65c9a04395
Jakub Steiner <jimmac@gmail.com>
2012-12-14 18:15:53 +0100

    use <media>, but provide a relative unit export to allow xinclude
    - height isn't respected when resizing, so sticking with <media> for now
    - relying on style="continues" for steps list

--- figures/render-figures.py
+++ figures/render-figures.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import glob
 import os
@@ -6,14 +6,31 @@ from xml.etree import ElementTree
 
 TEMP = './tmp.svg'
 
+def relativizeSize(fname):
+  global svg
+  
+  #calculate relative sizes if used as xinclude
+  root = svg.getroot()
+  width = int(root.get('width'))
+  height = int(root.get('height'))
+  root.set('width','100%')
+  root.set('height',str(round( height / width * 100, 2)) + "%")
+  root.set('viewBox',"0 0 %s %s" % (width, height))
+
 def rebrand(fname, brand):
-  svg = ElementTree.parse(fname)
+  global svg
+  
   for e in svg.iterfind(".//{http://www.w3.org/2000/svg}rect[@id='background']"):
     e.set('style', 'fill:url(#%s);' % (brand,))
-  svg.write(TEMP)
+  
 
 for fname in glob.glob('*svg'):
-  print fname
+  global svg
+  
+  print("processing %s" % (fname))
+  os.system("inkscape --vacuum-defs -l %s %s" % (TEMP, fname))
+  svg = ElementTree.parse(TEMP)
   rebrand(fname, "BLANK") #BLANK, GNOME, RHEL7
-  os.system("inkscape --vacuum-defs -l ../getting-started/C/figures/%s %s" % (fname, TEMP))
+  #relativizeSize(fname)
+  svg.write('../getting-started/C/figures/%s' % (fname))
   os.unlink(TEMP)
