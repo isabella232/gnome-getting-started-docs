180fcc527bc2f72a2e699f6fd16149914784c3ea
Jakub Steiner <jimmac@gmail.com>
2013-02-11 15:12:36 +0100

    use SVG for video thumbnails
    - scales better
    - translatable
    - branding

---
+++ animation/thumbs/render-thumbs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+from xml.etree import ElementTree
+
+TEMP = './tmp.svg'
+
+def relativizeSize(fname):
+  global svg
+  
+  #calculate relative sizes if used as xinclude
+  root = svg.getroot()
+  width = int(root.get('width'))
+  height = int(root.get('height'))
+  root.set('width','100%')
+  root.set('height',str(round( height / width * 100, 2)) + "%")
+  root.set('viewBox',"0 0 %s %s" % (width, height))
+
+def rebrand(fname, brand):
+  global svg
+  
+  for e in svg.iterfind(".//{http://www.w3.org/2000/svg}rect[@id='background']"):
+    e.set('style', 'fill:url(#%s);' % (brand,))
+  
+
+for fname in glob.glob('*svg'):
+  global svg
+  
+  print("processing %s" % (fname))
+  os.system("inkscape --vacuum-defs -l %s %s" % (TEMP, fname))
+  svg = ElementTree.parse(TEMP)
+  rebrand(fname, "GNOME") #BLANK, GNOME, RHEL7
+  #relativizeSize(fname)
+  svg.write('../../getting-started/C/%s' % (fname))
+  os.unlink(TEMP)
