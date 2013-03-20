e7b8f793c7451d53cd38a0e766b201dbcfcaf942
Jakub Steiner <jimmac@gmail.com>
2013-03-21 00:04:23 +0100

    rendered vids for switching and workspaces
    - changed due to the keyboard shortcuts
      https://bugzilla.gnome.org/show_bug.cgi?id=695532

--- animation/render-localized.sh
+++ animation/render-localized.sh
@@ -2,7 +2,7 @@ case $1 in
 	osx)
 	  export PYTHONPATH="/Users/jimmac/.blender/scripts"
           blender="/Applications/blender.app/Contents/MacOS/blender";;
-	linux)
+	*)
           export PYTHONPATH=$PYTHONPATH:/usr/lib64/python3.3/site-packages
 	  blender="blender";;
 esac
