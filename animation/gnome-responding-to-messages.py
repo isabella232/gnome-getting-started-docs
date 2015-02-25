23c8fcc8c6fab8a8bfcd879a6d68f4e59b62e665
Jakub Steiner <jimmac@gmail.com>
2015-02-25 18:00:46 +0100

    responding to messages: update for 3.16

--- animation/gnome-responding-to-messages.py
+++ animation/gnome-responding-to-messages.py
@@ -14,15 +14,15 @@ def typewriteit(scene):
     j = int((bpy.context.scene.frame_current-640)/2)
   else:
     j = 0
-  if bpy.context.scene.frame_current >= 1024:
-    k = int((bpy.context.scene.frame_current-1024)/2)
-    #print(k,typewrite3)
-  else:
-    k = 0
+  #if bpy.context.scene.frame_current >= 1024:
+  #  k = int((bpy.context.scene.frame_current-1024)/2)
+  #  #print(k,typewrite3)
+  #else:
+  #  k = 0
   #print(typewrite, i, typewrite[:i])
   bpy.data.objects['bubble.response'].data.body = typewrite[:i]
   bpy.data.objects['bubble.response2'].data.body = typewrite2[:j]
-  bpy.data.objects['bubble.response3'].data.body = typewrite3[:k]
+  #bpy.data.objects['bubble.response3'].data.body = typewrite3[:k]
 
 def main():
   
@@ -41,7 +41,7 @@ def main():
           bpy.data.objects[textobj.get('id')].data.body = textobj.text
       bpy.data.objects['typewriter'].data.body = t[lang].find('t[@id="bubble.response"]').text
       bpy.data.objects['typewriter2'].data.body = t[lang].find('t[@id="bubble.response2"]').text
-      bpy.data.objects['typewriter3'].data.body = t[lang].find('t[@id="bubble.response3"]').text
+      #bpy.data.objects['typewriter3'].data.body = t[lang].find('t[@id="bubble.response3"]').text
       #bpy.data.objects['user.mt.bubble'].data.body = bpy.data.objects['user'].data.body #needs to be left aligned :/
       gnomerender.render(lang)
       gnomerender.transcode(lang)
