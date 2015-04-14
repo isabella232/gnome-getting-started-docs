f5a608171c59fce2fc0425cc89b5c37c9b60bce2
Jakub Steiner <jimmac@gmail.com>
2015-04-14 16:30:47 +0200

    responding: few more fixes
    - fadeout of the selected item in keboard navigation scene
    - try to fix the damn typewriter

--- animation/gnome-responding-to-messages.py
+++ animation/gnome-responding-to-messages.py
@@ -3,17 +3,17 @@ from xml.etree import ElementTree as ET
 
 def typewriteit(scene):
   typewrite = bpy.data.objects['typewriter'].data.body
-  typewrite2 = bpy.data.objects['typewriter2'].data.body
-  typewrite3 = bpy.data.objects['typewriter3'].data.body
-  #psani zacina v sekvenci na 152
+  #typewrite2 = bpy.data.objects['typewriter2'].data.body
+  #typewrite3 = bpy.data.objects['typewriter3'].data.body
+  #psani zacina v sekvenci na 146
   if bpy.context.scene.frame_current >= 146:
-    i = int((bpy.context.scene.frame_current-146)/2)
+    i = int((bpy.context.scene.frame_current-146)/2) #letter every two frames
   else:
     i = 0
-  if bpy.context.scene.frame_current >= 640:
-    j = int((bpy.context.scene.frame_current-640)/2)
-  else:
-    j = 0
+  #if bpy.context.scene.frame_current >= 640:
+  #  j = int((bpy.context.scene.frame_current-640)/2)
+  #else:
+  #  j = 0
   #if bpy.context.scene.frame_current >= 1024:
   #  k = int((bpy.context.scene.frame_current-1024)/2)
   #  #print(k,typewrite3)
@@ -21,7 +21,7 @@ def typewriteit(scene):
   #  k = 0
   #print(typewrite, i, typewrite[:i])
   bpy.data.objects['bubble.response'].data.body = typewrite[:i]
-  bpy.data.objects['bubble.response2'].data.body = typewrite2[:j]
+  #bpy.data.objects['bubble.response2'].data.body = typewrite2[:j]
   #bpy.data.objects['bubble.response3'].data.body = typewrite3[:k]
 
 def main():
@@ -40,7 +40,7 @@ def main():
         if textobj.get('id') in bpy.data.objects: #prelozit jestli existuje jako index
           bpy.data.objects[textobj.get('id')].data.body = textobj.text
       bpy.data.objects['typewriter'].data.body = t[lang].find('t[@id="bubble.response"]').text
-      bpy.data.objects['typewriter2'].data.body = t[lang].find('t[@id="bubble.response2"]').text
+      #bpy.data.objects['typewriter2'].data.body = t[lang].find('t[@id="bubble.response2"]').text
       #bpy.data.objects['typewriter3'].data.body = t[lang].find('t[@id="bubble.response3"]').text
       #bpy.data.objects['user.mt.bubble'].data.body = bpy.data.objects['user'].data.body #needs to be left aligned :/
       gnomerender.render(lang)
